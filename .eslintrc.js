Bootstrap-Academy/frontend

[FEAT] {#94} Configured and installed ESLint

.eslintrc.js
@@ -1,0 +1,15 @@
+module.exports = {
+    extends: [
+        // 'eslint:recommended',
+        // 'plugin:vue/vue3-recommended',
+        // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+        'indent': ['warn', 2]
+    },
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    }
+}

monster-dev5/Bootstrap-Academy-Frontend

.eslintrc.js
@@ -1,0 +1,15 @@
+module.exports = {
+  extends: [
+    // 'eslint:recommended',
+    // 'plugin:vue/vue3-recommended',
+    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+  ],
+  rules: {
+    'indent': ['error', 2]
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    "parser": "@typescript-eslint/parser",
+    "sourceType": "module"
+  }
+}